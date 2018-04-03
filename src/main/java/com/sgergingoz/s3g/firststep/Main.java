f5c4f16fd15688c3634feac3bbb55d68381dc23c
sgergingoz <sgergingoz@VTEKLSGERGIN.vodafone.local>
2018-04-03 23:11:34 +0300

    initial

---
+++ src/main/java/com/sgergingoz/s3g/firststep/Main.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.sgergingoz.s3g.firststep;
+
+
+
+/**
+ * @author sgergingoz
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("S3G First Step project starting...");
+
+	}
+
+}
